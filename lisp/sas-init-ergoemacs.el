f10740890b169fd8c9c9863e294fb40683880e2c
Andranik Simonyan <andranik.s.s@gmail.com>
2016-04-28 00:24:59 +0300

    Add files

---
+++ lisp/sas-init-ergoemacs.el
@@ -0,0 +1,7 @@
+
+;(package-initialize)
+(require 'ergoemacs-mode)
+
+;(setq ergoemacs-theme nil) ;; Uses Standard Ergoemacs keyboard theme
+;(setq ergoemacs-keyboard-layout "us") ;; Assumes QWERTY keyboard layout
+;(ergoemacs-mode 1)
